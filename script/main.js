1ae6cdda2a9fd44c15815b3519f3b14ec8a4d7ef
Denys <gotsdo@gmail.com>
2017-12-23 19:27:25 +0200

    Sliders
    Main page and secondary pages sliders added

--- script/main.js
+++ script/main.js
@@ -33,26 +33,37 @@
 			}; 
 		}; 
 
-/* Buttons onclick handler */
+		/* Buttons onclick handler */
 
 		const handleRelocations = function(e) {
 			const triggerNode = e.target;
-			
-			switch (triggerNode.getAttribute("data-function")) {
-				case "nav-button":
-					const targetName = triggerNode.getAttribute("data-target");
-					const targetNodes = document.querySelectorAll('[data-name="'+targetName+'"]'); 
-					const targetNode = targetNodes[0];
-					const targetType = targetNode.getAttribute("data-type");
-					const sameNodeTypes = document.querySelectorAll('[data-type="'+targetType+'"]');
-					let triggerName; 
+			const triggerNodeFunction = triggerNode.getAttribute("data-function"); 
+			const targetName = triggerNode.getAttribute("data-target");
+			const targetNodes = document.querySelectorAll('[data-name="'+targetName+'"]'); 
+			const targetNode = targetNodes[0];
 
-					if (triggerNode.getAttribute("data-name")) {
-						triggerName = triggerNode.getAttribute("data-name");
-					}; 
-					
+			let targetType;
+			let triggerName; 
+			let triggerNodeSubfunction; 
+
+			if (targetNode && targetNode.hasAttribute("data-type")) {
+				targetType = targetNode.getAttribute("data-type");
+			}; 
+			
+			if (triggerNode && triggerNode.hasAttribute("data-name")) {
+				triggerName = triggerNode.getAttribute("data-name");
+			}; 
+
+			if (triggerNode && triggerNode.hasAttribute("data-subfunction")) {
+				triggerNodeSubfunction = triggerNode.getAttribute("data-subfunction");
+			}; 
+
+			const sameNodeTypes = document.querySelectorAll('[data-type="'+targetType+'"]');
+			
+			switch (triggerNodeFunction) { 
+				case "nav-button": {
 					switch (targetType) {
-						case "interactive-block":
+						case "interactive-block": {
 							handlePseudoArray(sameNodeTypes, function(obj) {
 								if (!obj.classList.contains("display-hidden")) {
 									obj.classList.add("display-hidden"); 
@@ -67,9 +78,10 @@
 								targetNode.scrollIntoView({block: "start", behavior: "smooth"});
 							}; 
 
-							break; 
+							break;
+						}
 
-						case "interactive-section": 
+						case "interactive-section": {
 							if (triggerName === targetName && targetNode.classList.contains("section-visible")) {
 								targetNode.classList.remove("section-visible");
 								outerWrapper.scrollIntoView({block: "start", behavior: "smooth"});
@@ -95,8 +107,9 @@
 							}; 
 
 							break; 
+						}
 
-						case "interactive-header-section":
+						case "interactive-header-section": {
 							if (targetNode.classList.contains("display-hidden")) {
 								handlePseudoArray(targetNodes, function(obj) {
 									obj.classList.remove("display-hidden");
@@ -110,35 +123,96 @@
 
 								targetNode.parentNode.classList.remove("header_nav-submenu-container-expand"); 
 							}; 
-							break; 
+							break;
+						}
 					};
 
 					break;
+				}
 
-				case "search-button":
+				case "search-button": {
 
 					break;
+				}
 
-				case "purchase-button":
+				case "purchase-button": {
 
-					break;  
+					break;
+				}
+
+				/* Main page and secondary pages sliders */
+
+				case "slide-arrow": {
+					const slides = handlePseudoArray(targetNode.querySelectorAll('[data-name="slide"]')); 
+					const slidesLength = slides.length; 
+
+					let currentSlide = slides.indexOf(slides.filter(function(elem) {
+						if (elem.classList.contains("slide-current")) {
+							return elem; 
+						};
+					})[0]);
+
+					const goToSlide = function(n) {
+						return (slidesLength + currentSlide + n) % slides.length;
+					}; 
+
+					switch(triggerNodeSubfunction) {
+						case "left-slide-arrow": {
+							slides[currentSlide].classList.add("slide-next");
+							slides[currentSlide].classList.remove("slide-current");
+
+							slides[goToSlide(1)].classList.add("slide-hidden");
+							slides[goToSlide(1)].classList.remove("slide-next");
+							
+							slides[goToSlide(-1)].classList.add("slide-current");
+							slides[goToSlide(-1)].classList.remove("slide-prev");
+
+							slides[goToSlide(-2)].classList.add("slide-prev");
+							slides[goToSlide(-2)].classList.remove("slide-hidden");
+
+							break;
+						}
+
+						case "right-slide-arrow": {
+							slides[currentSlide].classList.remove("slide-current");
+							slides[currentSlide].classList.add("slide-prev");
+												
+							slides[goToSlide(1)].classList.remove("slide-next");
+							slides[goToSlide(1)].classList.add("slide-current");
+							
+							slides[goToSlide(2)].classList.remove("slide-hidden");
+							slides[goToSlide(2)].classList.add("slide-next"); 
+							
+							slides[goToSlide(-1)].classList.remove("slide-prev");
+							slides[goToSlide(-1)].classList.add("slide-hidden");
+
+							break;
+						}
+					};
+					break;
+				}
 			};
+
 			e.preventDefault(); 
 			e.stopPropagation(); 
 		}; 
-		 
-/* Landing page slide show */
 
-		function landingPageSlideShow() {
+		attachListeners(buttons, handleRelocations);
+		 
+		/* Landing page slideshow */
+
+		const landingPageSlideShow = (function() {
 			const slides = document.getElementsByClassName("landing-page_slide"); 
 			const backgroundSlides = document.getElementsByClassName("landing-page_background");
 			const landingPageButton = document.getElementsByClassName("landing-page-button")[0]; 
+
 			let currentSlide = 0; 
 			let currentBackgroundSlide = 0;
 
 			const generatePosition = function() {
 				let randX = Math.round(Math.random() * 100);
 				let randY = Math.round(Math.random() * 100);
+
 				return ("background-position: " + randX + "% " + randY + "%")
 			};
 
@@ -147,7 +221,9 @@
 			const goToSlide = function(n) {
 				slides[currentSlide].classList.remove("landing-page_slide-visible");
 				backgroundSlides[currentSlide].classList.remove("landing-page_background-visible");
+
 				currentSlide = (n + slides.length) % slides.length;
+
 				slides[currentSlide].classList.add("landing-page_slide-visible");
 				slides[currentSlide].setAttribute("style", generatePosition()); 
 				backgroundSlides[currentSlide].classList.add("landing-page_background-visible");
@@ -157,53 +233,36 @@
 				goToSlide(currentSlide + 1); 
 			}; 
 
-			const slideInterval = setInterval(nextSlide, 3000);
+			const slideInterval = setInterval(nextSlide, 4000);
 
 			landingPageButton.addEventListener("click", function() {
 				clearInterval(slideInterval); 
 			}, false); 
-		}; 
-
-/* Main and secondary pages sliders */
-
-		function handleSliders() {
-			const sliders = document.querySelectorAll('[data-name="slider"]');
-
-			handlePseudoArray(sliders, function(obj) {
-				const leftButton = obj.querySelector('[data-name="left-slide-arrow"]'); 
-				const rightButton = obj.querySelector('[data-name="right-slide-arrow"]'); 
-				let slides = obj.querySelectorAll('[data-name="slide"]');
-				let currentSlide = 0; 
-
-				slides = handlePseudoArray(slides); 
-
-				const goToSlide = function(n) {
-					slides[currentSlide].classList.remove("slide-current");
-					slides[currentSlide].classList.add("slide-hidden");
-					currentSlide = (n + slides.length) % slides.length;
-					slides[currentSlide].classList.remove("slide-hidden", "slide-prev", "slide-next");
-					slides[currentSlide].classList.add("slide-current");
-					slides[currentSlide - 1].classList.remove("slide-hidden", "slide-prev", "slide-next");
-					slides[currentSlide - 1].classList.add("slide-prev");
-					slides[currentSlide + 1].classList.remove("slide-hidden", "slide-prev", "slide-next");
-					slides[currentSlide + 1].classList.add("slide-next");
-				};
-
-				const nextSlide = function() {
-					goToSlide(currentSlide + 1);
-				}; 
-
-				const previousSlide = function() {
-					goToSlide(currentSlide - 1);
-				}; 
-
-				rightButton.addEventListener("click", nextSlide); 
-				leftButton.addEventListener("click", previousSlide); 
-			}); 
-		}; 
-
-		attachListeners(buttons, handleRelocations);
-		landingPageSlideShow(); 
-		handleSliders(); // 
+		})();
 	}; 
 })(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* TODO: Constraints - pop up window on wrong form submition
+add this to form validation 
+var email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I expect an e-mail, darling!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
+*/
